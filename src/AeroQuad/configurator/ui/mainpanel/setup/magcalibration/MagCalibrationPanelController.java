3d911f20a951db6362c09bebb2e9f42d2284ccef
Kenny9999 <patricemainville@hotmail.com>
2014-03-04 22:08:13 -0500

    Receiver calibration stuff

--- src/AeroQuad/configurator/ui/mainpanel/setup/magcalibration/MagCalibrationPanelController.java
+++ src/AeroQuad/configurator/ui/mainpanel/setup/magcalibration/MagCalibrationPanelController.java
@@ -134,18 +134,25 @@ public class MagCalibrationPanelController implements IMagCalibrationPanelContro
         _panel.setZMaxValue(Integer.toString((int)_zMax));
     }
 
+    // AQ Conf = 5.000000,-93.000000,-21.500000
+
 
     private void updateCalValueToBoard()
     {
-        final double xMagBias = -((double)(_xMin +_xMax) / 2);
-        final double yMagBias = -((double)(_yMin +_yMax) / 2);
-        final double zMagBias = -((double)(_zMin +_zMax) / 2);
+        //final double xMagBias = -((_xMin +_xMax) / 2);
+        //final double yMagBias = -((_yMin +_yMax) / 2);
+        //final double zMagBias = -((_zMin +_zMax) / 2);
+
+        final double xMagBias = (_xMax+_xMin);
+        final double yMagBias = (_yMax+_yMin);
+        final double zMagBias = (_zMax+_zMin);
 
         final StringBuffer magBiasSetCommand = new StringBuffer();
         magBiasSetCommand.append("M ");
         magBiasSetCommand.append(xMagBias).append(";");
         magBiasSetCommand.append(yMagBias).append(";");
         magBiasSetCommand.append(zMagBias).append(";");
-        _communicator.sendCommand(magBiasSetCommand.toString());
+        System.out.println(magBiasSetCommand.toString());
+        //_communicator.sendCommand(magBiasSetCommand.toString());
     }
 }
